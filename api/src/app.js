8cd097a4dede4e957b3ac7053025597cabfa90e0
Christopher Bonner <cbonner.dev@outlook.com>
2025-04-21 01:41:18 +0100

    changed cors name

--- api/src/app.js
+++ api/src/app.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static("uploads"))
 app.use(morgan("dev"))
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://cheffy.christopher-bonner.com',
   credentials: true
 }));
 
